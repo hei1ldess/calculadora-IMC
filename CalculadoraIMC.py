e7d754052a80981f355e2b3fb97c80af57ad333f
Victor <83261965+hei1ldess@users.noreply.github.com>
2022-01-21 11:50:16 -0300

    Update CalculadoraIMC.py

--- CalculadoraIMC.py
+++ CalculadoraIMC.py
@@ -1,19 +1,19 @@
 Altura = float(input("Insira sua altura em cm:"))
 Peso = float(input("Insira seu peso em kg:"))
 
-IMC = Peso / (Altura/100)**2
+IMC = Peso / Altura**2
 
-print(f"Seu IMC é {IMC}")
+print(f"Seu IMC é {IMC:.2f}")
 
 if IMC <= 18.4:
-  print('Está abaixo do peso, BORA ENGORDAR PORRA')
+ print('Está abaixo do peso, BORA ENGORDAR PORRA')
 elif IMC <= 24.9:
-  print('Está no seu peso normal, MANTÉM CARALHO')
+ print('Está no seu peso normal, MANTÉM CARALHO')
 elif IMC <= 29.9:
-  print('TA UM POUCO GORDO EM FILHO DA PUTA')
+ print('TA UM POUCO GORDO EM FILHO DA PUTA')
 elif IMC <= 34.9:
-  print('TA PORRA É UM ELEFANTE')
+ print('TA PORRA É UM ELEFANTE')
 elif IMC <= 39.9:
-  print('COMEÇA A FAZER DIETA E ACADEMIA GORDO SAFADO')
+ print('COMEÇA A FAZER DIETA E ACADEMIA GORDO SAFADO')
 elif IMC >= 40.0:
-  print('FAZ UMA BARIATRICA') 
+ print('FAZ UMA BARIATRICA') 
